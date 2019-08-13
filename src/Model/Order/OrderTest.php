6af14581efc0995f70d373af2b07a24979dd9040
Chris Harrison <chris.harrison@funeralzone.co.uk>
2019-08-13 09:49:45 +0100

    Apply results of modelling session

---
+++ src/Model/Order/OrderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class OrderTest extends TestCase
+{
+    public function test_order_amount_not_negative() {
+
+    }
+
+    public function test_order_amount_cannot_be_zero() {
+
+    }
+}
